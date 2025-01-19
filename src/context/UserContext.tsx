ae7ce10d46d38458aeeb3b9d268bafb017413873
harivenkatesh05 <venkateshponnusami@gmail.com>
2025-01-19 16:57:16 +0530

    Refactor UserContext to remove phone number verification logic and unused imports

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,9 +1,7 @@
 'use client';
 
-import { getUserDetails, updatePhone } from '@/app/apis';
+import { getUserDetails } from '@/app/apis';
 import React, { createContext, useContext, useState, ReactNode, useEffect } from 'react';
-import GooglePhoneVerification from '@/components/auth/GooglePhoneVerification';
-import { toast } from 'react-hot-toast';
 
 interface UserContextType {
 	user: User | null;
@@ -15,18 +13,10 @@ const UserContext = createContext<UserContextType | undefined>(undefined);
 
 export const UserProvider = ({ children }: { children: ReactNode }) => {
 	const [user, setUser] = useState<User | null>(null);
-	const [showPhoneModal, setShowPhoneModal] = useState(false);
 	
 	useEffect(() => {
 		// Check if there is already a user
 		if(user) {
-			// if (!user.phoneNumber) {
-			// 	setShowPhoneModal(true);
-			// }
-
-			// else if(!user.phoneNumberVerfied) {
-			// 	setShowPhoneNumberVerificationModal(true)
-			// }
 			return
 		};
 
@@ -34,23 +24,6 @@ export const UserProvider = ({ children }: { children: ReactNode }) => {
 			setUser(user);
 		});
 	}, [user]);
-
-	const handlePhoneSubmit = async (phoneNumber: string) => {
-		try {
-			if (!user?.email) return;
-			
-			const data = await updatePhone(user.email, phoneNumber);
-			if (data.user) {
-				setUser(data.user);
-				setShowPhoneModal(false);
-				toast.success('Phone number updated successfully');
-			}
-		// eslint-disable-next-line @typescript-eslint/no-unused-vars
-		} catch (error) {
-			console.error(error);
-			toast.error('Failed to update phone number');
-		}
-	};
 	
 	return (
 		// <UserContext.Provider value={{ user, setUser }}>
