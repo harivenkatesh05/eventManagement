a7a57acd3af63629ab083dcbc18b5feb7d976569
harivenkatesh05 <venkateshponnusami@gmail.com>
2025-01-20 01:13:35 +0530

    preconnect fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,6 +45,8 @@ export default function RootLayout({
 	return (
 		<html lang="en" className={roboto.className}>
 			<body>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+			 	<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
 				<GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
 					<div className="d-flex flex-column h-100">
 						<ClientRouteHandler>
