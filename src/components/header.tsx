dbf8af59de40539bd3d38796f96e6c6c740b3dbc
harivenkatesh05 <venkateshponnusami@gmail.com>
2025-01-20 12:47:35 +0530

    hamburger icon design changed

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -28,8 +28,10 @@ export default function Header() {
 				<nav className="navbar navbar-expand-lg bg-barren barren-head navbar fixed-top justify-content-sm-start pt-0 pb-0">
 					<div className="container">
 						<button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-							<span className="navbar-toggler-icon">
-								<i className="fa-solid fa-bars"></i>
+							<span className='icon'>
+								<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 50 50">
+									<path d="M 0 7.5 L 0 12.5 L 50 12.5 L 50 7.5 Z M 0 22.5 L 0 27.5 L 50 27.5 L 50 22.5 Z M 0 37.5 L 0 42.5 L 50 42.5 L 50 37.5 Z"></path>
+								</svg>
 							</span>
 						</button>
 						<Link className="navbar-brand order-1 order-lg-0 ml-lg-0 ml-2 me-auto" href="/">
